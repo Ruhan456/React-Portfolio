3853031dd6dccd81390929e21d8e028a0e3c5132
Ruhan Bhavsar <63120454+Ruhan456@users.noreply.github.com>
2025-01-12 23:53:57 -0500

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ const Home = ({ darkMode }) => {
                 <a href="https://www.linkedin.com/in/ruhan-bhavsar-43a074292/" target="_blank" rel="noopener noreferrer"><img src="../images/linkedin.png" alt="Linkedin" style={{ width: '20px', height: '20px', transition: 'transform 0.3s ease', marginRight: '5px', paddingBottom:'10px'}} /> </a>
                 <a href="https://github.com/Ruhan456" target="_blank" rel="noopener noreferrer"><img src="../images/github.png" alt="github" style={{ width: '22px', height: '22px', transition: 'transform 0.3s ease', paddingBottom:'8px' }} /></a>
                 <p className={`home-description ${darkMode ? "dark-text" : "light-text"}`} >
-                    Hi, I'm Ruhan. This is my portfolio, where I have displayed my technical projects and skills, as well as many ways to contact me. <br /> <br /> I am currently a Mechatronics Engineer at the University of Waterloo, with great interest in learning more about machine learning and embedded systems
+                                        Hi, I'm Ruhan. This is my portfolio, where I have displayed my technical projects and skills, as well as many ways to contact me. <br /> <br /> I am currently an undergraduate student at the University of Waterloo studying Mechatronics Engineering, with great interest in learning more about machine learning and embedded systems.
                 </p>
             </div>
         </section>
